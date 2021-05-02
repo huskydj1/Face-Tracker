702b8a6dc6b3b518780c2d759d21e612d369eb61
Davin Jeong <husky.davin@gmail.com>
2021-05-01 21:35:18 -0700

    Explicit L2 Norm (Haven't figured out numpy linalg norm yet)

--- greedymatching.py
+++ greedymatching.py
@@ -10,7 +10,9 @@ class face(object):
         return str(self.frame_num)
 
     def L2Dist(self, other):
-        return np.linalg.norm(self.landmarks-other.landmarks)
+        c = self.landmarks - other.landmarks
+        return np.sum(np.sqrt(np.sum(np.square(c), axis = 1)))
+        # return np.linalg.norm(self.landmarks-other.landmarks)
 
 class sustained(object):
     def __init__(self):
