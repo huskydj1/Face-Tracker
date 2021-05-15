26605fe01620c91cb890f05fc15f66a11e42de4b
Davin Jeong <husky.davin@gmail.com>
2021-05-14 23:04:32 -0700

    Update when newest outputs

--- VGGFace2_facetracker.py
+++ VGGFace2_facetracker.py
@@ -47,11 +47,15 @@ def track(inputFileFolder, inputFileName, mtcnn):
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     fourcc1 = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(find_available_file(name=("outputVideos/OUTPUT_" + inputFileName)), 
+    out = cv2.VideoWriter(find_available_file(name=("outputVideos/VGTOUT_" + inputFileName)), 
         fourcc1, fps, (frame_width, frame_height))
 
     #Matching Class
     matching = Matching()
+
+    #Frame by Frame Information:
+    
+    debugFile = open("VGTINFO_{}.txt".format(inputFileName), "w")
 
     #Read frames
     frame_num = 0
@@ -64,8 +68,10 @@ def track(inputFileFolder, inputFileName, mtcnn):
 
             if not(face_array is None):
                 boxes, probs, landmarks = mtcnn.detect(frame, landmarks = True)
-                id_list = matching.update_batch(face_array, frame_num)
+                id_list, scores = matching.update_batch(face_array, frame_num)
                 drawframe.notate(frame, boxes, landmarks = landmarks, probs = probs, faceids=id_list)
+                
+                debugFile.write(str(frame_num) + ": " + str(id_list) + " " + str(scores) + '\n')
 
             # Frame by Frame Info (Coupled with outputVideos\onepersonwalking_output12.mp4)
             # Frame #, Face Detected (bool), trackList string representation (face class identified by frame #)
@@ -78,6 +84,8 @@ def track(inputFileFolder, inputFileName, mtcnn):
         else:
             break
         
+    
+    debugFile.close()
     cap.release()
     out.release()
     cv2.destroyAllWindows()
@@ -86,7 +94,7 @@ def track(inputFileFolder, inputFileName, mtcnn):
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 if torch.cuda.is_available():
     print(torch.cuda.get_device_name(0))
-print('Running on device: {}'.format(device))
+print('Running on device: {}'.format(device))   
 
 mtcnn = MTCNN(keep_all = True, device = device)
 '''img = cv2.imread("sourceVideos/muskbezos.jpg")
@@ -96,4 +104,10 @@ face_array, prob = mtcnn(img, return_prob = True)
 for i, face in enumerate(face_array):
     cv2.imshow("image" + str(i), face.permute(1, 2, 0).numpy()); cv2.waitKey(0); cv2.destroyAllWindows(); cv2.waitKey(1)
 '''
+
 track("sourceVideos", "onemanonewoman_face-demographics-walking-and-pause", mtcnn)
+'''track("sourceVideos", "dogrunning", mtcnn)
+track("sourceVideos", "oneman_face-demographics-walking-and-pause", mtcnn)
+track("sourceVideos", "onemanonewoman_face-demographics-walking-and-pause", mtcnn)
+track("sourceVideos", "onemantwowomen_face-demographics-walking-and-pause", mtcnn)
+track("sourceVideos", "onewoman_face-demographics-walking-and-pause", mtcnn)'''
--- VGGFace2_matching.py
+++ VGGFace2_matching.py
@@ -3,11 +3,13 @@ import cv2
 import torch
 import numpy as np
 from scipy.spatial.distance import cosine
+from sklearn.preprocessing import normalize
+
 from facenet_pytorch import InceptionResnetV1
 
 class Face(object):
     def __init__(self, embedding, frame_num, id):
-        self.embedding = embedding
+        self.embedding = embedding  
         self.frame_num = frame_num
         self.id = id
 
@@ -33,20 +35,29 @@ class Matching(object):
         return embeddings
     
     def match_score(self, known_embedding, new_embedding):
+        '''known_embedding = normalize(known_embedding.reshape(1, -1))
+        new_embedding = normalize(new_embedding.reshape(1, -1))'''
         score = cosine(known_embedding, new_embedding)
         return score
     
-    def update_batch(self, face_array, frame_num, thresh = 0.6):
+   #def is_match(self, known_embedding, new_embedding, thresh):
+   #     return self.match_score(known_embedding, new_embedding) <= thresh
+    
+    def update_batch(self, face_array, frame_num, thresh = 0.5):
         embeddings = self.get_embeddings(face_array)
 
         id_list = []
+        scores = []
 
         for embedding in embeddings:
+            scores.append([])
+
             best_i = -1
             best_score = 100
             for i, person in enumerate(self.prev_data):
                 known_face = person[-1]
                 cur_score = self.match_score(known_face.embedding, embedding)
+                scores[-1].append(cur_score)
                 # print(cur_score)
                 if cur_score < best_score:
                     best_score = cur_score
@@ -60,4 +71,4 @@ class Matching(object):
                 new_face = Face(embedding, frame_num, len(self.prev_data))
                 self.prev_data.append([new_face])
 
-        return id_list
+        return [id_list, scores]
